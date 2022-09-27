2f10dd14bd2581edf93487db31790e0da6993498
Saurab Harish Prabhakar <saurab@ualberta.ca>
2022-09-26 19:40:14 -0600

    All except deep no end work so far

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-#  coding: utf-8 
 import socketserver
+import os
+import datetime
+
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,12 +30,166 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+    def status_405(self):
+        print("405 INVOKED")
+        data = "HTTP/1.1 405 Method Not Allowed \r\n"
+        data += "Date:"
+        data += datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        data += "\r\n"
+        data += "Connection: close\r\n"
+        data += "\r\n\r\n"
+        
+
+        return data
+
+    def status_400(self):
+        print("405 INVOKED")
+        data = "HTTP/1.1 400 Bad Request \r\n"
+        data += "Date:"
+        data += datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        data += "\r\n"
+        data += "Connection: close\r\n"
+        data += "\r\n\r\n"
+        return data
+
+    def status_200(self, content_type, file_content):
+        print("200 INVOKED")  
+        data = "HTTP/1.1 200 OK\r\n" # from class notes
+        content_type_string = "Content-Type: " + content_type
+        data += content_type_string
+        data += "\r\n\r\n"
+        data += file_content.decode('utf-8')
+        data += "\r\n"
+
+        return data
+
+
+    def status_301(self, relativePath):
+        data = "HTTP/1.1 301 Moved Permanently\r\n" # from class notes
+        data += "Date:"
+        data += datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        data += "\r\n"
+        data += "Connection: close\r\n"
+        location = relativePath+"/"
+        location_string = "Location: " + location
+        data += location_string
+        data += "\r\n"
+        data += "Content-Length: 0\r\n"
+        data += "\r\n\r\n"
+
+        return data
+
+    def status_404(self):
+        data = "HTTP/1.1 404 Not Found\r\n"
+        data += "Date:"
+        data += datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        data += "\r\n"
+        data += "Connection: close\r\n"
+        data += "\r\n\r\n"
+
+        return data    
+
+#--------------------------------------------------------------------------------------------------------
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
 
+
+        self.decode_data = self.data.decode('utf-8') # Decoded request
+        httpRequest = self.decode_data.split('\n')[0]
+        # self.request.sendall(bytearray("OK",'utf-8'))
+        if httpRequest[:4] != "GET ":
+            response = self.status_405() # Method not allowed
+            self.request.sendall(bytearray(response, 'utf-8'))
+            print("405 NOT ALLOWED")
+            #exit()
+
+        elif len(httpRequest.split(' ')) != 3:
+            response = self.status_400() # missing or too many arguments
+            self.request.sendall(bytearray(response, 'utf-8'))
+            print("400 Bad Response")
+            #exit()
+
+        else: #valiud continue
+            request_arr = [x.strip() for x in httpRequest.strip('[]').split(' ')]
+            print("REQUEST ARRAY: ",request_arr)
+
+            _ = request_arr[0]
+            resource = request_arr[1]
+            http_ver = request_arr[2]
+            print(resource)
+
+            path = 'www'
+
+            print("PATH",path)
+            print("RESOURCE",resource)
+            path += resource
+            print("FINAL given PATH:", path)
+        
+            if not os.path.exists(path):
+                response = self.status_404() # Not found
+                print("404 Not Found")
+                self.request.sendall(bytearray(response, 'utf-8'))
+                
+                #exit() # freetests case 4
+            else:
+                #if ends in / ... else if file specified
+                if resource[-1] == '/': #append index.html
+                    path += "/index.html"
+                    mime_type = 'text/html'
+
+                    if not os.path.exists(path): # if assumed index.html DNE
+                        response = self.status_404() # Not Found
+                        print("404 Not Found")
+                        self.request.sendall(bytearray(response, 'utf-8'))
+                        
+                        #exit()
+                    else:
+                        with open(path, 'rb') as fp:
+                            buf = fp.read()
+                            print("file read and loaded")
+
+                            if (path.endswith(".html")):
+                                mime_type = "text/html"
+                            elif (path.endswith(".css")):
+                                mime_type = "text/css"
+
+                        response = self.status_200(mime_type, buf)
+                        print("RESPONSE 200 file assumed and loaded")
+                        self.request.sendall(bytearray(response, 'utf-8'))
+                
+                        #exit() # freetests case 2
+
+                else: # else a file is specified
+                    with open(path, 'rb') as fp:
+                        buf = fp.read()
+                        print("file read and loaded")
+
+                        if (path.endswith(".html")):
+                            mime_type = "text/html"
+                        elif (path.endswith(".css")):
+                            mime_type = "text/css"
+                            print("MIMETYPE:", mime_type)
+                            #exit()
+
+                        response = self.status_200(mime_type, buf)
+                        print("RESPONSE 200 file request recognized")
+                        self.request.sendall(bytearray(response, 'utf-8'))
+                    
+                        #exit() #freetests case 1
+                
+                        
+
+
+
+
+            
+
+
+
+
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
